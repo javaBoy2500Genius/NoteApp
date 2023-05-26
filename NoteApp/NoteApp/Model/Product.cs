e8d301552a746c8aff74f27bf4f19654a2d2dc94
javaBoy2500Genius <127086143+seniorSmodar@users.noreply.github.com>
2023-05-26 16:00:09 +0600

    yes

---
+++ NoteApp/NoteApp/Model/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoteApp.Model
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+
+        public double prise { get; set; }
+
+        public int count { get; set; }
+
+        public double total { get { return prise * count; } }
+
+        public DateTime CreateDate { get; set; }    
+
+
+
+    }
+}
